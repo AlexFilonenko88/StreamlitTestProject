09a3b436fafa0f3cb6b0f3bb2476690ff3de0117
Alex <af88_88@mail.ru>
2026-07-29 14:26:39 +0300

    Add data. Change view dashbord

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 
 st.title('Dahboard продаж')
-st.write('Приложение обновилось!')
+st.write('Приложение обновилось')
+
+df = pd.read_csv('data.csv')
+st.dataframe(df)
+st.write('Количество строк', len(df))
+st.write('Количество столбцов', len(df.columns))
