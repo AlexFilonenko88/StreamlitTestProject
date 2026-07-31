a35bda2b66a585ca877502739f15428f913276ea
Alex <af88_88@mail.ru>
2026-07-31 15:48:29 +0300

    Added check correct file structure

--- pages/raw_data.py
+++ pages/raw_data.py
@@ -8,25 +8,34 @@ file = st.file_uploader(
     type=['xlsx', 'csv']
 )
 
+required_columns = [
+    'product',
+    'price',
+    'category'
+]
+
 if file is not None:
     if file.name.endswith('.csv'):
         df = pd.read_csv(file)
     else:
         df = pd.read_excel(file)
 
-    st.session_state['df'] = df
+    if not all(column in df.columns for column in required_columns):
+        st.error('Некорректная структура файла')
+    else:
+        st.session_state['df'] = df
 
-    price_by_categories = df.groupby('category').price.sum().reset_index()
+        price_by_categories = df.groupby('category').price.sum().reset_index()
 
-    col_1, col_2 = st.columns(2)
+        col_1, col_2 = st.columns(2)
 
-    with col_1:
-        st.dataframe(df)
+        with col_1:
+            st.dataframe(df)
 
-    with col_2:
-        st.dataframe(price_by_categories)
+        with col_2:
+            st.dataframe(price_by_categories)
 
-    with st.expander('Подробная информация'):
-        st.write(df.describe())
+        with st.expander('Подробная информация'):
+            st.write(df.describe())
 else:
     st.write('Данные еще не загружены')
