9cdb47135f47aecee400a4355a84a3cad67487fa
Alex <af88_88@mail.ru>
2026-08-01 11:24:30 +0300

    Delete error import

--- pages/carts.py
+++ pages/carts.py
@@ -1,30 +1,28 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-from app import category
+
 
 if 'df' in st.session_state:
     df = st.session_state['df']
 
     if 'category' not in st.session_state:
         category = 'Electronics'
-    st.session_state['category'] = category
+    #st.session_state['category'] = category
 
     category = st.selectbox('Категории', df.category.unique())
-
     st.session_state['category'] = category
-
 
     filtered_df = df[df['category'] == category]
 
-    fig, ax = plt.subplots()
-    ax.hist(filtered_df.price)
+    #fig, ax = plt.subplots()
+    #ax.hist(filtered_df.price)
 
     tab_1, tab_2 = st.tabs(['Гистограмма', 'Линейный график'])
     with tab_1:
         fig, ax = plt.subplots()
         ax.hist(filtered_df.price)
         st.pyplot(fig)
-    with tab_1:
+    with tab_2:
         st.line_chart(filtered_df.price)
 else:
     st.write('Загрузите данные')
